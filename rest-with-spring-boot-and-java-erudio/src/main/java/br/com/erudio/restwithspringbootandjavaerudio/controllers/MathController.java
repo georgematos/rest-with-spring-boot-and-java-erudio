8a3d6c9c10865a22bbdd19ed6a31d57c1dbdb5d1
George Matos <georgemattos@gmail.com>
2023-05-23 15:09:06 -0300

    Number converter created

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controllers/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controllers/MathController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.erudio.restwithspringbootandjavaerudio.converters.NumberConverter;
 import br.com.erudio.restwithspringbootandjavaerudio.exceptions.UnsupportedMathOperationException;
 
 @RestController
@@ -15,55 +16,42 @@ public class MathController {
 
     @GetMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        return NumberConverter.convertToDouble(numberOne) + NumberConverter.convertToDouble(numberTwo);
     }
 
     @GetMapping("/sub/{numberOne}/{numberTwo}")
     public Double sub(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+        return NumberConverter.convertToDouble(numberOne) - NumberConverter.convertToDouble(numberTwo);
     }
 
     @GetMapping("/mult/{numberOne}/{numberTwo}")
     public Double mult(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return convertToDouble(numberOne) * convertToDouble(numberTwo);
+        return NumberConverter.convertToDouble(numberOne) * NumberConverter.convertToDouble(numberTwo);
     }
 
     @GetMapping("/div/{numberOne}/{numberTwo}")
     public Double div(@PathVariable String numberOne, @PathVariable String numberTwo) throws Exception {
-        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return convertToDouble(numberOne) / convertToDouble(numberTwo);
+        return NumberConverter.convertToDouble(numberOne) / NumberConverter.convertToDouble(numberTwo);
     }
 
     @GetMapping("/squareRoot/{number}")
     public Double div(@PathVariable String number) throws Exception {
-        if(!isNumeric(number)) {
+        if (!NumberConverter.isNumeric(number)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return Math.sqrt(convertToDouble(number));
-    }
-
-    public Double convertToDouble(String strNumber) {
-        if(strNumber == null) return 0D;
-        String number  = strNumber.replaceAll(",", ".");
-        if (isNumeric(number)) return Double.parseDouble(number);
-        return 0D;
-    }
-
-    public boolean isNumeric(String strNumber) {
-        if (strNumber == null) return false;
-        String number  = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+        return Math.sqrt(NumberConverter.convertToDouble(number));
     }
 
 }
