0b7799495103a39062c35fead89e59ac7dfa053d
George Matos <georgemattos@gmail.com>
2023-05-26 15:11:36 -0300

    refactor on findAll method

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -6,23 +6,25 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.erudio.restwithspringbootandjavaerudio.models.Person;
 import br.com.erudio.restwithspringbootandjavaerudio.services.PersonService;
 
 @RestController
+@RequestMapping("/person")
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
-    @GetMapping("/person")
-    public List<Person> getPersons() {
-        return personService.getPersons();
+    @GetMapping
+    public List<Person> findAllPersons() {
+        return personService.findAll();
     }
 
-    @GetMapping("/person/{id}")
+    @GetMapping("/{id}")
     public Person getPersonById(@PathVariable String id) {
         return personService.getById(UUID.fromString(id));
     }
--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/services/PersonService.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/services/PersonService.java
@@ -15,30 +15,34 @@ public class PersonService {
     private Logger logger = Logger.getLogger(PersonService.class.getName());
     private List<Person> persons = new ArrayList<>();
 
-    public List<Person> getPersons() {
+    public List<Person> findAll() {
         logger.info("Finding all persons!");
-        Person person1 = new Person.Builder()
-                .withFirstName("George")
-                .withLastName("Matos")
-                .withAddress("Rua Oriente, 121, Piratininga")
-                .withGender("M").build();
-        Person person2 = new Person.Builder()
-                .withFirstName("Thaiane")
-                .withLastName("Matos")
-                .withAddress("Rua Oriente, 121, Piratininga")
-                .withGender("F").build();
-        Person person3 = new Person.Builder()
-                .withFirstName("Moana")
-                .withLastName("Matos")
-                .withAddress("Rua Oriente, 121, Piratininga")
-                .withGender("D").build();
-
-        persons.addAll(Arrays.asList(person1, person2, person3));
+        persons.addAll(mockPersons());
         return persons;
     }
 
     public Person getById(UUID id) {
         logger.info("Finding one person!");
         return persons.stream().filter((x) -> x.getId().equals(id)).findFirst().get();
+    }
+
+    private List<Person> mockPersons() {
+        Person person1 = new Person.Builder()
+                .withFirstName("George")
+                .withLastName("Matos")
+                .withAddress("Rua Oriente, 121, Piratininga")
+                .withGender("Male").build();
+        Person person2 = new Person.Builder()
+                .withFirstName("Thaiane")
+                .withLastName("Matos")
+                .withAddress("Rua Oriente, 121, Piratininga")
+                .withGender("Female").build();
+        Person person3 = new Person.Builder()
+                .withFirstName("Moana")
+                .withLastName("Matos")
+                .withAddress("Rua Oriente, 121, Piratininga")
+                .withGender("Dog").build();
+
+        return Arrays.asList(person1, person2, person3);
     }
 }
