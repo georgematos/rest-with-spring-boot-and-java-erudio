0378a999a3709f5961f1449f623858ea544da469
George Matos <georgemattos@gmail.com>
2023-06-02 10:22:15 -0300

    Person mapped for Entity and PersonRepository created

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/models/Person.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/restwithspringbootandjavaerudio/models/Person.java
@@ -3,12 +3,33 @@ package br.com.erudio.restwithspringbootandjavaerudio.models;
 import java.util.Objects;
 import java.util.UUID;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public class Person {
 
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     private UUID id;
+    
+    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+    
+    @Column(name = "last_name", length = 80)
     private String lastName;
+
+    @Column(nullable = false, length = 100)
     private String address;
+
+    @Column(nullable = false, length = 6)
     private String gender;
 
     public Person() {
